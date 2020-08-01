f0999ea42a1fc94ce9bd36a08de734a611e9a158
Airtoc <dani0909lol@gmail.com>
2020-07-31 21:46:48 -0500

    0.1 Prueba

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,5 +1,20 @@
 package launcher;
 
-public class Launcher {
+import java.awt.EventQueue;
 
-}
+import view.Window;
+
+public class Launcher {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Window frame = new Window();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}	
